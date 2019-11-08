311486d57a6d4cce2ac9ce619a46f3c0873b8209
zicy <git@zicy.dk>
2019-11-08 23:34:31 +0100

    Correction to output from external script

--- DiscordCommands/StatusCommand.py
+++ DiscordCommands/StatusCommand.py
@@ -5,19 +5,18 @@ from discord.ext import commands
 
 def run(Bot, config, GUILD, CHANNEL_ID):
 
-    #Bot = self.bot
-
     @Bot.command(name='status')
     async def _cmd(ctx):
         if ctx.message.guild.id == int(GUILD):
             if ctx.message.channel.id == int(CHANNEL_ID):
-                cmd = config['SCRIPT']['Path'] + "test.cmd"
-                if os.path.exists(cmd):
-                    await ctx.send("Running " + cmd +  " ...")
-                    msg = os.popen(cmd).read()
-                    await ctx.send("'''" + msg + "'''")
+                CMD_PATH = config['SCRIPT']['Path']
+                CMD = "test.cmd"
+                if os.path.exists(CMD_PATH):
+                    await ctx.send("Running " + CMD +  " ...")
+                    msg = os.popen(CMD_PATH, CMD).read()
+                    await ctx.send("```" + msg + "```")
                 else:        
-                    await ctx.send("Error! " + cmd + " don't exist")
+                    await ctx.send("Error! " + CMD_PATH + " don't exist")
 
     @_cmd.error
     async def _cmd_error(ctx, error):
