e954af44abace4164ef89ce416cafc3ea2b897a1
zicy <git@zicy.dk>
2019-11-08 20:18:56 +0100

    initial commit

---
+++ DiscordBot.py
@@ -0,0 +1,48 @@
+import configparser
+import discord
+from discord.ext import commands
+from DiscordCommands import *
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('settings.cfg')
+
+TOKEN = config['BOT']['Token']
+GUILD = config['BOT']['Guild']
+CHANNEL_ID = config['BOT']['Channel_ID']
+
+DESCRIPTION = '''Im here to help'''
+VERSION = "CraftersBot V0.1-dev"
+
+Bot = commands.Bot(command_prefix="!", description=DESCRIPTION,case_insensitive=True)
+
+@Bot.event
+async def on_ready():
+    print("Starting Bot,")
+    print("Version: " + VERSION)
+    print("Username: " + Bot.user.name)
+    print("Bot ID: " + str(Bot.user.id))
+    print("---[ Connected Servers]---")
+    for Guild in Bot.guilds:
+        print("Guild ID: " + str(Guild.id) + " Name: " + Guild.name + " Member Count: " + str(Guild.member_count))
+    print("---[ Bot Started ]---")
+    await Bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name=VERSION, type=1))
+    print("")
+
+
+# !Ping
+PingCommand.run(Bot, GUILD, CHANNEL_ID)
+
+# !Rcon [server] command
+RconCommand.run(Bot, GUILD, CHANNEL_ID)
+
+# !Restart <server> <delay>
+RestartCommand.run(Bot, GUILD, CHANNEL_ID)
+
+# !Status [server]
+StatusCommand.run(Bot, GUILD, CHANNEL_ID)
+
+
+
+
+Bot.run(TOKEN)
---
+++ DiscordCommands/RconCommand.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+def run(Bot, GUILD, CHANNEL_ID):
+
+    #Bot = self.bot
+
+    @Bot.command(name="rcon")
+    async def _cmd(ctx):
+        if ctx.message.guild.id == int(GUILD):
+            if ctx.message.channel.id == int(CHANNEL_ID):
+                await ctx.send("Pong")
+
+    @_cmd.error
+    async def _cmd_error(ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Unknown error!")
+    
